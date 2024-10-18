8a6c2ce0721716e1fc7f6dcd5b38f3a6e65ccade
How Chuen Yu <kylejustkyle@gmail.com>
2024-10-18 07:44:24 +0200

    Second commit

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Pineapple on pizza is heresy!");
+        System.out.println("I might not exist...");
     }
 }
