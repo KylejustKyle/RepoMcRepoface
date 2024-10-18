d9ba371decc1710ff477a636ede680d71b3bac36
How Chuen Yu <kylejustkyle@gmail.com>
2024-10-18 11:05:10 +0200

    One more

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
 
         System.out.println("Pineapple on pizza is heresy!");
         System.out.println("The only true programming language is assembly!");
-        System.System.out.println("this is not a line of code");
+        System.out.println("this is not a line of code");
+        System.out.println("You can never stop me putting pineapples on pizza!");
     }
 }
