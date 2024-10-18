edc82ac5a5accfd446b09f5ec7b4544f611d9f43
How Chuen Yu <kylejustkyle@gmail.com>
2024-10-18 08:02:03 +0200

    Assembly!

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Pineapple on pizza is heresy!");
+        System.out.println("The only true programming language is assembly!");
     }
 }
