a8d99b013156939107e6353f098889171141421d
How Chuen Yu <kylejustkyle@gmail.com>
2024-10-18 07:46:36 +0200

    Revert "Second commit"
    This reverts commit 8a6c2ce0721716e1fc7f6dcd5b38f3a6e65ccade.

--- Main.java
+++ Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Pineapple on pizza is heresy!");
-        System.out.println("I might not exist...");
     }
 }
